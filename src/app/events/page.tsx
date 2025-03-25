c3e17d144cf83a257e6f3f11e9d57458b37d0c1f
gavrielsatrio <gavrielsatrio@gmail.com>
2025-03-25 21:11:37 +0700

    feat: fetch data from notion API
    All events page

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,17 +1,54 @@
 'use client';
 
 import { useRouter } from "next/navigation";
+import { useNotion } from "@/hooks/useNotion";
+import { useEffect, useState } from "react";
+
+import { IPage } from "@/interfaces/notions/IPage";
+import { IEvent } from "@/interfaces/models/IEvent";
+
+import { getDateRange } from "@/utils/notions/Date";
 
 import { Event } from "@/components/cards/Event";
 import { Container } from "@/components/Container";
 import { ChevronLeft } from "@/icons/ChevronLeft";
+import { ComponentState } from "@/states/ComponentState";
+
+interface EventsPageState extends ComponentState {
+  events: Array<IPage<IEvent>>,
+}
 
 function Events() {
   const router = useRouter();
+  const { getAllEvents } = useNotion();
+
+  const [data, setData] = useState<EventsPageState>({
+    loading: true,
+    events: []
+  });
+
+  const fetchEvents = async () => {
+    setData(prev => ({
+      ...prev,
+      loading: true
+    }));
+
+    const events = await getAllEvents();
+
+    setData(prev => ({
+      ...prev,
+      loading: false,
+      events
+    }));
+  }
 
   const handleBack = () => {
     router.back();
   }
+
+  useEffect(() => {
+    fetchEvents();
+  }, []);
 
   return (
     <Container className="bg-[#FAFAFA] min-h-dvh px-16 py-16">
@@ -24,13 +61,18 @@ function Events() {
         <p className="mt-2">New events can be added from Notion</p>
 
         <div className="grid grid-cols-2 mt-6 gap-6">
-          <Event name="CFXX" date="09/11/2025 - 10/11/2025" location="ICE BSD" type="Mail Order" orderCount={30} packedCount={5} paidCount={9} pendingCount={16} />
-          <Event name="CFXX" date="09/11/2025 - 10/11/2025" location="ICE BSD" type="Mail Order" orderCount={30} packedCount={5} paidCount={9} pendingCount={16} />
-          <Event name="CFXX" date="09/11/2025 - 10/11/2025" location="ICE BSD" type="Mail Order" orderCount={30} packedCount={5} paidCount={9} pendingCount={16} />
-          <Event name="CFXX" date="09/11/2025 - 10/11/2025" location="ICE BSD" type="Mail Order" orderCount={30} packedCount={5} paidCount={9} pendingCount={16} />
-          <Event name="CFXX" date="09/11/2025 - 10/11/2025" location="ICE BSD" type="Mail Order" orderCount={30} packedCount={5} paidCount={9} pendingCount={16} />
-          <Event name="CFXX" date="09/11/2025 - 10/11/2025" location="ICE BSD" type="Mail Order" orderCount={30} packedCount={5} paidCount={9} pendingCount={16} />
-          <Event name="CFXX" date="09/11/2025 - 10/11/2025" location="ICE BSD" type="Mail Order" orderCount={30} packedCount={5} paidCount={9} pendingCount={16} />
+          {data.events.map(event => (
+            <Event
+              name={event.properties['Name'].title[0].plain_text}
+              date={getDateRange(event.properties['Event Date'].date, 'DD/MM/YYYY')}
+              location={event.properties['Location'].rich_text.reduce((prev, curr) => `${prev} ${curr.plain_text}`, '')}
+              type={event.properties['Type'].select.name}
+              orderCount={30}
+              packedCount={5}
+              paidCount={9}
+              pendingCount={16}
+              key={event.id} />
+          ))}
         </div>
       </div>
     </Container>
