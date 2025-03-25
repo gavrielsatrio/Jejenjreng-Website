a8fbb2e371a72b49a4e7c986ab6be6a3b7ecc4dd
gavrielsatrio <gavrielsatrio@gmail.com>
2025-03-25 15:44:43 +0700

    feat: add home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,48 @@
+'use client';
+
 import Image from "next/image";
+import { useEffect } from "react";
 
-export default function Home() {
+import { Event } from "@/components/cards/Event";
+import { Container } from "@/components/Container";
+
+function App() {
+  const fetchEvents = async () => {
+    
+  }
+
+  useEffect(() => {
+    fetchEvents();
+  }, []);
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <Container className="bg-[#FAFAFA] min-h-dvh px-16 pt-16">
+      <title>Jejenjreng Website</title>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <div className="col-span-4 max-h-[calc(100vh-64px)] overflow-y-hidden">
+        <h1 className="font-black text-5xl">Jejenjreng</h1>
+        <p className="mt-4 text-black/60">Sampaikanlah dariku, walau hanya satu ayat</p>
+        <Image src='/assets/jeje.png' alt="Jeje Portrait" width={1228} height={2160} className="object-contain w-full h-[calc(100%-64px)] p-12" />
+      </div>
+      <div className="col-span-8 max-h-[calc(100dvh-64px)] overflow-y-auto">
+        <div className="flex items-center justify-between mb-6">
+          <h2 className="font-bold text-3xl">Upcoming Events</h2>
+          <a href="/events">Manage all events</a>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+        <div className="flex flex-col gap-y-6">
+          <Event name="CFXX" date="09/11/2025 - 10/11/2025" location="ICE BSD" type="Pickup" orderCount={30} packedCount={5} paidCount={9} pendingCount={16} />
+        </div>
+
+        <div className="flex items-center justify-between mb-6 mt-10">
+          <h2 className="font-bold text-3xl">Past Events</h2>
+        </div>
+        <div className="flex flex-col gap-y-6 mb-16">
+          <Event name="CFXX" date="09/11/2025 - 10/11/2025" location="ICE BSD" type="Mail Order" orderCount={30} packedCount={5} paidCount={9} pendingCount={16} />
+          <Event name="CFXX" date="09/11/2025 - 10/11/2025" location="ICE BSD" type="Mail Order" orderCount={30} packedCount={5} paidCount={9} pendingCount={16} />
+        </div>
+      </div>
+    </Container>
+  )
 }
+
+export default App;
