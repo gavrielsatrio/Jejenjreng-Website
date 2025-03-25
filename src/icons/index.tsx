429333bcd4c265b13b5d23c553d70ac18772dea9
gavrielsatrio <gavrielsatrio@gmail.com>
2025-03-25 15:44:23 +0700

    feat: add icons

---
+++ src/icons/index.tsx
@@ -0,0 +1,19 @@
+import { Box } from "./Box"
+import { Map } from "./Map"
+import { Calendar } from "./Calendar"
+import { ChevronLeft } from "./ChevronLeft"
+import { Envelope } from "./Envelope"
+import { Truck } from "./Truck"
+import { Phone } from "./Phone"
+import { Receipt } from "./Receipt"
+
+export {
+  Box,
+  Map,
+  Calendar,
+  ChevronLeft,
+  Envelope,
+  Truck,
+  Phone,
+  Receipt
+}
