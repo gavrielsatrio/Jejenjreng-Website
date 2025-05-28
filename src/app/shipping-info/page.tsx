241adf9f19c810d5b09572ff84ab4c9e56ea0d18
gavrielsatrio <gavrielsatrio@gmail.com>
2025-05-28 13:07:38 +0700

    fix: dummy shipping info

--- src/app/shipping-info/page.tsx
+++ src/app/shipping-info/page.tsx
@@ -2,7 +2,7 @@ import { ShippingInfo } from "@/components/ShippingInfo";
 
 function ShippingInfoPage() {
   return (
-    <ShippingInfo></ShippingInfo>
+    <ShippingInfo eventName="Test Event" recipient="Test" address="Test" phoneNumber="081234567890"></ShippingInfo>
   )
 }
 
